c0998b7994a1220a90b5afce30890dedbf8ef4c9
Unknown <mosquitojoe85@gmail.com>
2019-08-03 23:56:50 +0800

    [CHANGE] 增加造型與按鈕

--- src/App.js
+++ src/App.js
@@ -1,17 +1,26 @@
 import React from 'react';
 import disc from './assets/disc.jpg';
 import './App.scss';
+import Button from './components/Button';
+import next from './assets/next.svg';
 
 function App() {
   return (
     <div className="App">
       <main className="playerBlock">
         <div className="top">
-          <div className="disc">
-            <img alt="disc image" src={disc} />
-            <div className="discCover"></div>
+          <div className="discBlock">
+            <div className="disc">
+              <img alt="disc image" src={disc} />
+              <div className="discCover"></div>
+            </div>
+            <div className="discLine1" />
+            <div className="discLine2" />
+            <div className="discLine3" />
           </div>
-          <div className="actBTnBlock"></div>
+          <div className="actBTnBlock">
+            <Button icon={next} act={() => {}} />
+          </div>
         </div>
         <div className="bottom"></div>
       </main>
---
+++ src/components/Button/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import './index.scss';
+
+function Button({ icon, act }) {
+  return (
+    <button className="btn"></button>
+  );
+}
+
+export default Button;
